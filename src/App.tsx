1d42c76796d6f9b711decd2929530e55ae45cec2
FrontendDev <uiux@okdollar.com>
2026-08-01 14:08:36 +0630

    project setup with shadcn ui

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import {Button} from "@/components/ui/button"
+
+function App() {
+
+  const handleClick = () => {
+    console.log("Button clicked")
+  }
+  
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button className="cursor-pointer" onClick={handleClick}>Click me</Button>
+    </div>
+  )
+}
+
+export default App;
